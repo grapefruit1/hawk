ff44556a2cd1d500a28c7e524979dc04b2ede0b5
kiseido <brettseguin@gmail.com>
2017-10-22 15:23:48 -0700

    tweaks to the base and the start of an engine

--- JSGame/JS/main.js
+++ JSGame/JS/main.js
@@ -1,5 +1,26 @@
 window.onload = customOnload;
 
-function customOnload(e) {
-	console.log(e);
+function customOnload() {
+	console.log(arguments,this);
+	var HawkInstance = HAWKJSFactory.factory();
+	HawkInstance.run();
 }
+
+var HAWKJSFactory = {factory:function(){
+	var HAWKJS = {};
+	HAWKJS.stats = {};
+	HAWKJS.stats.frameCount = 0;
+	HAWKJS.version = 0.1;
+	HAWKJS.desiredFPS = 60;
+	HAWKJS.run = function(){
+		this.intervalID = setInterval(this.update, 1000 / this.desiredFPS);
+		console.log(arguments,this);
+	};
+	HAWKJS.update = function(){
+		//console.log(arguments,this);
+		console.log("update run");
+		//this.stats.frameCount++;
+		clearInterval(this.intervalID);
+	};
+	return HAWKJS;
+}};
